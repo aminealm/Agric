cda0a0c13ba2f22c2be48e967707931be4fcb2c6
unknown <aminealm7@gmail.com>
2026-07-02 14:31:49 +0100

    change home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Home.css";
 import Typewriter from "typewriter-effect";
 import agric1 from "../../img/agric1.jpg";
@@ -6,7 +6,39 @@ import agric2 from "../../img/agric2.jpg";
 import agric3 from "../../img/agric3.jpg";
 import { Link } from "react-scroll";
 
+const slides = [
+  {
+    image: agric1,
+    alt: "D\u00e9veloppement agricole et territorial",
+    interval: 5000,
+  },
+  {
+    image: agric2,
+    alt: "Agriculture et d\u00e9veloppement rural",
+    interval: 3000,
+  },
+  {
+    image: agric3,
+    alt: "Environnement et ressources naturelles",
+    interval: 3000,
+  },
+];
+
 function Home() {
+  const [activeSlide, setActiveSlide] = useState(0);
+
+  const showSlide = (index) => {
+    setActiveSlide((index + slides.length) % slides.length);
+  };
+
+  useEffect(() => {
+    const timer = window.setTimeout(() => {
+      setActiveSlide((current) => (current + 1) % slides.length);
+    }, slides[activeSlide].interval);
+
+    return () => window.clearTimeout(timer);
+  }, [activeSlide]);
+
   return (
     <div className="container-fluide home" id="home">
       <div className="home-content">
@@ -17,7 +49,7 @@ function Home() {
             options={{
               strings: [
                 "Agriconsulting Maroc",
-                "L’expertise au service du développement agricole et territorial",
+                "L\u2019expertise au service du d\u00e9veloppement agricole et territorial",
               ],
               autoStart: true,
               loop: true,
@@ -26,9 +58,7 @@ function Home() {
         </h1>
 
         <p>
-          Agriconsulting Maroc accompagne les opérateurs publics et privés dans
-          leurs décisions stratégiques et opérationnelles, au service du
-          développement durable au Maroc et en Afrique.
+          {"Agriconsulting Maroc accompagne les op\u00e9rateurs publics et priv\u00e9s dans leurs d\u00e9cisions strat\u00e9giques et op\u00e9rationnelles, au service du d\u00e9veloppement durable au Maroc et en Afrique."}
         </p>
 
         <Link
@@ -39,81 +69,51 @@ function Home() {
           duration={100}
           className="btn-main home__cta"
         >
-          Découvrir plus
+          {"D\u00e9couvrir plus"}
         </Link>
       </div>
 
-      <div
-        id="carouselExampleCaptions"
-        className="carousel carousel-home slide"
-        data-bs-ride="carousel"
-      >
+      <div id="carouselExampleCaptions" className="carousel carousel-home slide">
         <div className="carousel-indicators">
-          <button
-            type="button"
-            data-bs-target="#carouselExampleCaptions"
-            data-bs-slide-to="0"
-            className="active"
-            aria-current="true"
-            aria-label="Slide 1"
-          />
-
-          <button
-            type="button"
-            data-bs-target="#carouselExampleCaptions"
-            data-bs-slide-to="1"
-            aria-label="Slide 2"
-          />
-
-          <button
-            type="button"
-            data-bs-target="#carouselExampleCaptions"
-            data-bs-slide-to="2"
-            aria-label="Slide 3"
-          />
+          {slides.map((slide, index) => (
+            <button
+              type="button"
+              className={index === activeSlide ? "active" : ""}
+              aria-current={index === activeSlide ? "true" : undefined}
+              aria-label={`Slide ${index + 1}`}
+              onClick={() => showSlide(index)}
+              key={slide.image}
+            />
+          ))}
         </div>
 
         <div className="carousel-inner">
-          <div className="carousel-item active" data-bs-interval="5000">
-            <img
-              src={agric1}
-              className="d-block w-100"
-              alt="Développement agricole et territorial"
-            />
-          </div>
-
-          <div className="carousel-item" data-bs-interval="3000">
-            <img
-              src={agric2}
-              className="d-block w-100"
-              alt="Agriculture et développement rural"
-            />
-          </div>
-
-          <div className="carousel-item" data-bs-interval="3000">
-            <img
-              src={agric3}
-              className="d-block w-100"
-              alt="Environnement et ressources naturelles"
-            />
-          </div>
+          {slides.map((slide, index) => (
+            <div
+              className={`carousel-item ${index === activeSlide ? "active" : ""}`}
+              data-bs-interval={slide.interval}
+              key={slide.image}
+            >
+              <img src={slide.image} className="d-block w-100" alt={slide.alt} />
+            </div>
+          ))}
         </div>
 
         <button
           className="carousel-control-prev"
           type="button"
-          data-bs-target="#carouselExampleCaptions"
-          data-bs-slide="prev"
+          onClick={() => showSlide(activeSlide - 1)}
+          aria-label="Pr\u00e9c\u00e9dent"
         >
           <span className="carousel-control-prev-icon" aria-hidden="true" />
-          <span className="visually-hidden">Précédent</span>
+          <span className="visually-hidden">{"Pr\u00e9c\u00e9dent"}</span>
         </button>
 
         <button
           className="carousel-control-next"
           type="button"
-          data-bs-target="#carouselExampleCaptions"
-          data-bs-slide="next"
+          onClick={() => showSlide(activeSlide + 1)}
+          aria-label="Suivant"
         >
           <span className="carousel-control-next-icon" aria-hidden="true" />
           <span className="visually-hidden">Suivant</span>
